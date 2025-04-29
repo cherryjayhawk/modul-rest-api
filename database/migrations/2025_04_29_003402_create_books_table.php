30b51261a642ac3cdec9969efd7d087e24a7357b
cherryjayhawk <muhbintangr19@gmail.com>
2025-04-29 19:08:04 +0700

    update

--- database/migrations/2025_04_29_003402_create_books_table.php
+++ database/migrations/2025_04_29_003402_create_books_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->string('author');
+            $table->string('publisher');
             $table->year('year');
-            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
--- database/seeders/BookSeeder.php
+++ database/seeders/BookSeeder.php
@@ -2,17 +2,81 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Book;
 
 class BookSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        Book::factory()->count(3)->create();
+        Book::create([
+            'title'     => 'Laskar Pelangi',
+            'author'    => 'Andrea Hirata',
+            'publisher' => 'Bentang Pustaka',
+            'year'      => 2005,
+        ]);
+
+        Book::create([
+            'title'     => 'Sang Pemimpi',
+            'author'    => 'Andrea Hirata',
+            'publisher' => 'Bentang Pustaka',
+            'year'      => 2006,
+        ]);
+
+        Book::create([
+            'title'     => 'The Hobbit',
+            'author'    => 'J. R. R. Tolkien',
+            'publisher' => 'George Allen & Unwin',
+            'year'      => 1937,
+        ]);
+
+        Book::create([
+            'title'     => 'The Lord of the Rings',
+            'author'    => 'J. R. R. Tolkien',
+            'publisher' => 'George Allen & Unwin',
+            'year'      => 1954,
+        ]);
+
+        Book::create([
+            'title'     => 'Harry Potter and the Philosopher\'s Stone',
+            'author'    => 'J. K. Rowling',
+            'publisher' => 'Bloomsbury',
+            'year'      => 1997,
+        ]);
+
+        Book::create([
+            'title'     => 'Harry Potter and the Chamber of Secrets',
+            'author'    => 'J. K. Rowling',
+            'publisher' => 'Bloomsbury',
+            'year'      => 1998,
+        ]);
+
+        Book::create([
+            'title'     => 'To Kill a Mockingbird',
+            'author'    => 'Harper Lee',
+            'publisher' => 'J. B. Lippincott & Co.',
+            'year'      => 1960,
+        ]);
+
+        Book::create([
+            'title'     => 'Go Set a Watchman',
+            'author'    => 'Harper Lee',
+            'publisher' => 'HarperCollins',
+            'year'      => 2015,
+        ]);
+
+        Book::create([
+            'title'     => '1984',
+            'author'    => 'George Orwell',
+            'publisher' => 'Secker & Warburg',
+            'year'      => 1949,
+        ]);
+        
+        Book::create([
+            'title'     => 'Animal Farm',
+            'author'    => 'George Orwell',
+            'publisher' => 'Secker & Warburg',
+            'year'      => 1945,
+        ]);
     }
 }
